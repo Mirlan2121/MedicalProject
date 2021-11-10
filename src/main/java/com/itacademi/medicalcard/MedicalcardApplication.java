e146c509f4560270992fba0f56dff959afc655b4
Alexander <89515014507@mail.ru>
2021-11-10 11:41:42 +0600

    firs commit

---
+++ src/main/java/com/itacademi/medicalcard/MedicalcardApplication.java
@@ -0,0 +1,13 @@
+package com.itacademi.medicalcard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalcardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalcardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itacademi/medicalcard/MedicalcardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itacademi.medicalcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalcardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
