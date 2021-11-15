20179dbf154baa48bc30dfc61bfac6b695f960b4
Alexander <89515014507@mail.ru>
2021-11-15 06:37:37 +0600

    проба гет запроса обьекта

--- src/main/java/com/example/Medical/Project/controller/PatUserInfoController.java
+++ src/main/java/com/example/Medical/Project/controller/PatUserInfoController.java
@@ -3,21 +3,19 @@ package com.example.Medical.Project.controller;
 import com.example.Medical.Project.entity.PatUserInfo;
 import com.example.Medical.Project.entity.User;
 import com.example.Medical.Project.service.impl.PatUserInfoServiceImpl;
-import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller("/patuserinfo")
+@Controller("/patUserinfo")
 public class PatUserInfoController {
     @Autowired
     private PatUserInfoServiceImpl patUserInfoService;
 
     @GetMapping
     public String getAllPatUsersInfo(Model model){
-        model.addAttribute("log","Вы вошли");
-
-        return "patuserinfo";
+        System.out.println("ЗАХОДИТ");
+        return "patUserinfo";
     }
 }
