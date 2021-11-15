2b6e86d9b4d91cad2813d4a39c60d54c62538a93
Alexander <89515014507@mail.ru>
2021-11-15 06:54:15 +0600

    Не удачные попытки передать обьект на Бэк

--- src/main/java/com/example/Medical/Project/controller/PatUserInfoController.java
+++ src/main/java/com/example/Medical/Project/controller/PatUserInfoController.java
@@ -7,15 +7,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller("/patUserinfo")
 public class PatUserInfoController {
     @Autowired
     private PatUserInfoServiceImpl patUserInfoService;
 
-    @GetMapping
+    @GetMapping("/patUserinfo")
     public String getAllPatUsersInfo(Model model){
         System.out.println("ЗАХОДИТ");
         return "patUserinfo";
+    }
+    @PostMapping("/patUserinfo")
+    public String setInfo(@RequestBody PatUserInfo userInfo, Model model){
+        System.out.println(userInfo);
+        return "/";
     }
 }
